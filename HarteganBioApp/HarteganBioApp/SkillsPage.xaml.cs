36510c9b5cfee2db6d48d212e748c88217aed999
Elizabeth Hartegan <ehartegan@gus.pittstate.edu>
2018-10-31 17:36:19 -0500

    Updated Skills Page

--- HarteganBioApp/HarteganBioApp/SkillsPage.xaml.cs
+++ HarteganBioApp/HarteganBioApp/SkillsPage.xaml.cs
@@ -13,22 +13,47 @@ namespace HarteganBioApp
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class SkillsPage : ContentPage
 	{
-        public ObservableCollection<string> Skills { get; set; }
+        public ObservableCollection<Skill> Skills { get; set; }
 
-		public SkillsPage ()
-		{
-			InitializeComponent ();
-
-            this.Skills = new ObservableCollection<string>();
-		}
-
-        private void Button_Clicked(object sender, EventArgs e)
+        public SkillsPage()
         {
-            this.Skills.Add(skillEntry.Text);
+            InitializeComponent();
 
-            skillEntry.Text = "";
+            this.Skills = new ObservableCollection<Skill>();
 
-            skillsList.ItemsSource = this.Skills;
+
+            Skill visualBasic = new Skill
+            {
+                Description = "Basic knowledge working with Visual Basic",
+                Name = "Visual Basic",
+                ImageURL = "VisualBasic.png"
+            };
+            this.Skills.Add(visualBasic);
+
+            Skill C = new Skill
+            {
+                Description = "Basic knowledge working with C++",
+                Name = "C++",
+                ImageURL = "C.png"
+            };
+            this.Skills.Add(C);
+
+            Skill Csharp = new Skill
+            {
+                Description = "Little knowledge working with C#",
+                Name = "C#",
+                ImageURL = "CSharp.png"
+            };
+            this.Skills.Add(Csharp);
+
+            this.skillsList.ItemsSource = this.Skills;
+        }
+
+
+        public class Skill {
+            public string ImageURL { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
         }
     }
 }
